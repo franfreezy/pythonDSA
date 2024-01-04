946b08dff4c5845aa8c8b1eea22937b611f24371
franfreezy <frandelwanjawa@yahoo.com>
2024-01-04 21:18:34 +0300

    few tweaks here and there, finito

--- R3.py
+++ R3.py
@@ -5,13 +5,15 @@ print('A program that outputs largest and smallest number among n numbers')
 
 n= int(input('How many numbers do you want to input:'))
 myArray=[]
-
+x=1
 def minmax(n):
+    global x
     while n>0:
-        data=(int(input('enter number '+ str(n)+ ' :' )))
+        data=(int(input('enter number '+ str(x)+ ' :' )))
         myArray.append(data)
        
         n= n-1
+        x=x+1
         data1=myArray[0]
         data2=myArray[0]
     for i in range(len(myArray)):
