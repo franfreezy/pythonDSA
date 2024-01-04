7ac650f9a52e1e6704d66b2ce8aa3dece098f2e9
franfreezy <frandelwanjawa@yahoo.com>
2024-01-04 21:15:52 +0300

    working model of little honest work

---
+++ R3.py
@@ -0,0 +1,27 @@
+#using a fn minmax(data) check for the largest or smallest number in n numbers
+#no use of inbuilt functions
+
+print('A program that outputs largest and smallest number among n numbers')
+
+n= int(input('How many numbers do you want to input:'))
+myArray=[]
+
+def minmax(n):
+    while n>0:
+        data=(int(input('enter number '+ str(n)+ ' :' )))
+        myArray.append(data)
+       
+        n= n-1
+        data1=myArray[0]
+        data2=myArray[0]
+    for i in range(len(myArray)):
+        data=myArray[i]
+        if(data1 > data):
+            data1 = data
+        if( data > data2):
+            data2 = data
+
+    print('the smallest number is: ' + str(data1))
+    print('the largest number is: '+str(data2))
+
+minmax(n)
