cd219ceb75dcff7fa3c7f9a239cc070861dd45a6
franfreezy <frandelwanjawa@yahoo.com>
2024-01-11 23:59:14 +0300

    additional

--- LL6.py
+++ LL6.py
@@ -8,3 +8,32 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head=None
+
+    #a method to print
+    def print(self):
+        if self.head is None:
+            print('linked list is empty')
+            return
+        itr=self.head
+        llstr=''
+        while itr.next:
+            llstr += str(itr.data)
+            itr=itr.next
+        print(llstr)
+
+
+    def addAtTheEnd(self,data):
+        if self.head is None:
+            self.head=Node(data,None)
+            return
+
+        itr=self.head
+        while itr.next:
+            itr=itr.next
+        itr.next=Node(data,None)
+        
+    def addAList(self,dataList):
+        self.head=None
+        for data in dataList:
+        
+    def removeFirst(self, index,data)
