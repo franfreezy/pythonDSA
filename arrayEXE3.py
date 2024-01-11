1b45c1c346771f4bad4f871709cb85f77e82e1d8
franfreezy <frandelwanjawa@yahoo.com>
2024-01-11 14:06:15 +0300

    done ex3

--- arrayEXE3.py
+++ arrayEXE3.py
@@ -1,2 +1,18 @@
 #Create a list of all odd numbers between 1 and a max number. 
 # Max number is something you need to take from a user using input() function
+
+#THE IMPLEMENTATION
+
+print('a program that prints odd numbers ')
+
+num=int(input('Enter the max number you are interested with:'))
+mylist=[]
+def OddFinder(num,mylist):
+    for i in range (num):
+        if (i %2) != 0:
+            mylist.append(i)
+        
+    return mylist
+
+print(OddFinder(num,mylist))
+
