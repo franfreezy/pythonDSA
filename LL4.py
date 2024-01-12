441aa3d41c5c39da83c21a1c691f99142ae271ec
franfreezy <frandelwanjawa@yahoo.com>
2024-01-12 12:52:57 +0300

    first of the noon

--- LL4.py
+++ LL4.py
@@ -7,4 +7,27 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head=None
+
+    def print(self):
+        if self.head is None:
+            print('linked list is empty')
+            return
+        
+        itr=self.head
+        llstr='' # initialise an empty string
+        while itr:
+            llstr += str(itr.data)+ '---'
+            itr=itr.next
+        print(llstr) #this string prints our llinked list sperated by the lines for delimination
+
+    def addTobeginning(self,data):
+        node = Node(data,self.head)
+        self.head=node
+
+    
+
+if __name__=='__main__':
+    ll=LinkedList()
+    ll.addTobeginning(1)
+    ll.print()
         
