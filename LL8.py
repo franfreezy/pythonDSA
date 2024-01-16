294c48f5ccf4f116c876089ab7321f513a3c7531
franfreezy <frandelwanjawa@yahoo.com>
2024-01-16 21:01:22 +0300

    lets go

--- LL8.py
+++ LL8.py
@@ -2,7 +2,7 @@
 #8. Write a Python program to create a doubly linked list, 
 # append some items and iterate through the list (print forward).
 class Node:
-    def __init__(self,data,next,prev):
+    def __init__(self,data):
         self.data=data
         self.next=next
         self.prev=prev
