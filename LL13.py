2e7434ef06e07c864be9088abf5d67bbc9fe2cad
franfreezy <frandelwanjawa@yahoo.com>
2024-01-17 01:17:18 +0300

    final for the night

--- LL13.py
+++ LL13.py
@@ -1,11 +1,12 @@
 #13. Write a Python program to search a specific item in a given doubly linked list 
 # and return true if the item is found otherwise return false.
 class Node:
-    def __init__(self,data,next,prev):
+    def __init__(self,data):
         self.data=data
-        self.next=next
-        self.prev
+        self.next=None
+        self.prev=None
 
 class LinkedList:
     def __init__(self):
         self.head=None
+        self.tail=None
