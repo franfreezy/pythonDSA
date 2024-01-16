a7dd9adc3bc068580d4bbbc05d0167cc73c28ccb
franfreezy <frandelwanjawa@yahoo.com>
2024-01-16 22:08:24 +0300

    additional

--- LL9.py
+++ LL9.py
@@ -2,9 +2,18 @@
 class Node:
     def __init__(self,data,next,prev):
         self.data=data
-        self.next=next
-        self.prev=prev
+        self.next=None
+        self.prev=None
 
 class LinkedList:
     def __init__(self):
         self.head=None
+        self.tail=None
+        self.size=0 #initialises to 0
+    
+    def append(self,data):
+        pass
+    def prepend(self,data):
+        pass
+    def print(self):
+        pass
