44eb7b914ef2212d627c593c65e94ac3c4d415d5
franfreezy <frandelwanjawa@yahoo.com>
2024-01-16 22:16:32 +0300

    additional

--- LL9.py
+++ LL9.py
@@ -12,7 +12,19 @@ class LinkedList:
         self.size=0 #initialises to 0
     
     def append(self,data):
-        pass
+        if self.head is None:
+            new_node=Node(data)
+            self.head=new_node
+            self.tail=new_node
+            return
+        else:
+            current=self.tail
+            current.next=new_node #next value after the tail is our data
+            new_node.prev=current #doubly links our list, the prev value to our data is self.tail
+            self.tail=new_node # our data becomes self.tail
+            
+        
+        
     def prepend(self,data):
         pass
     def print(self):
