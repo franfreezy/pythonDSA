a0073def0f68bc8f8438ee51f469458d2e18d715
franfreezy <frandelwanjawa@yahoo.com>
2024-01-17 14:35:24 +0300

    additional

--- LL14.py
+++ LL14.py
@@ -1,12 +1,41 @@
 #14. Write a Python program to delete a specific item from a given doubly linked list.
 class Node:
-    def __init__(self,data,next,prev):
+    def __init__(self,data):
         self.data=data
         self.next=None
         self.prev=None
 
-class LinkedList:
+class dLinkedList:
     def __init__(self):
         self.head=None
         self.tail=None
-        
+    def append(self,data):
+        node=Node(data)
+        if self.head is None:
+            self.head=node
+            self.tail=node
+            return
+        else:
+            self.tail.next=node
+            node.prev=self.tail
+            self.tail=node
+
+    def print(self):
+        current=self.head
+        dllstr=''
+        while current:
+            dllstr+=str(current.data)+'----'
+            current=current.next
+        print (dllstr)
+  ##implementation      
+    def Remove(self, data):
+        pass
+
+if __name__=='__main__':
+    dll=dLinkedList()
+    dll.append(6)
+    dll.append(23)
+    dll.append(61)
+    dll.append(67)
+    dll.print()
+    
