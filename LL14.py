bc0fdb0f97de339dc24c048395908c18f5d9b47b
franfreezy <frandelwanjawa@yahoo.com>
2024-01-17 14:05:59 +0300

    additional

--- LL14.py
+++ LL14.py
@@ -2,10 +2,11 @@
 class Node:
     def __init__(self,data,next,prev):
         self.data=data
-        self.next=next
-        self.prev=prev
+        self.next=None
+        self.prev=None
 
 class LinkedList:
     def __init__(self):
         self.head=None
+        self.tail=None
         
