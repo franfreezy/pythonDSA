2b126f94cc94fef571d3f0e3dcd5e9f21003beee
franfreezy <frandelwanjawa@yahoo.com>
2024-01-17 13:22:18 +0300

    first of the noon

--- LL13.py
+++ LL13.py
@@ -6,7 +6,23 @@ class Node:
         self.next=None
         self.prev=None
 
-class LinkedList:
+class dLinkedList:
     def __init__(self):
         self.head=None
         self.tail=None
+
+    def append(self, data):
+        pass
+
+    def print(self):
+        pass
+
+    def search(self,data):
+        pass
+
+if __name__=='__main__':
+    dll=dLinkedList()
+    dll.append(6)
+    dll.append(23)
+    dll.append(61)
+    dll.append(67)
