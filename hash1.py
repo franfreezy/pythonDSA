d7571b5af393c4dfe3385647a868fe90c943aea1
franfreezy <frandelwanjawa@yahoo.com>
2024-01-19 08:29:41 +0300

    lets go

--- hash1.py
+++ hash1.py
@@ -1,11 +1,12 @@
-#nyc_weather.csv contains new york city weather for first few days in the month of January. 
+#nyc_weather.csv contains new york city weather for first few days in the month of
+# January. 
 #Write a program that can answer following,
 #What was the average temperature in first week of Jan
 #What was the maximum temperature in first 10 days of Jan
 #Figure out data structure that is best for this problem
 
 import pandas as pd
-pd.__version__
+
 
 df=pd.read_csv(r"nyc_weather.csv")
 print(df)
