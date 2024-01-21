036a35c510c677a485e6500f0b62bc2c7b446102
franfreezy <frandelwanjawa@yahoo.com>
2024-01-21 18:25:18 +0300

    additional

--- hash1.py
+++ hash1.py
@@ -7,37 +7,33 @@
 
 import pandas as pd
 
-
-def dataFrameToArray(df):
-    myArray=[]
-    for index,row in df.iterrows():
-        date = row['Date']
-        temp = row['temp']
-        data=date,temp
-        myArray.append(data)
-    return myArray
-# we can now use hash maps
-def ArrayToElements(new_arr):
-    key=[]
-    value=[]
-    for row in new_arr:
-        key.append(row[0])
-        value.append(row[1])
-    return key  
-    
-new_arr=dataFrameToArray(df)
-ArrayToElements(new_arr)
-
-
-#we can now create a class to take care of the hash table stuffs
+#employing OOP
 class hashTable:
     def __init__(self) -> None:
         self.max_value=10
         self.arr=[None for i in range(self.max_value)]
+
     def processCsv(self,DataFile):
         df=pd.read_csv(DataFile)
         df=df.rename(columns={'date':'Date','temperature(F)':'temp'})
         return df
+
+    def dataFrameToArray(self, df):
+        myArray=[]
+        for index,row in df.iterrows():
+            date = row['Date']
+            temp = row['temp']
+            data=date,temp
+            myArray.append(data)
+        return myArray
+
+    def ArrayToElements(self,new_arr):
+        key=[]
+        value=[]
+        for row in new_arr:
+            key.append(row[0])
+            value.append(row[1])
+        return key  
 
     #this is the hash function, converts the key to an index for the table
     def get_hash(self,key):
@@ -51,7 +47,9 @@ class hashTable:
      
 if __name__=='__main__':
     item=hashTable()
-
+    DataFile="nyc_weather.csv"
+    print(item.processCsv(DataFile))
+    
 
 
 
