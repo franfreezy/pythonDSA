8f24e468e3be9762c9c8796028e79b36520d9195
franfreezy <frandelwanjawa@yahoo.com>
2024-01-22 20:15:42 +0300

    additional

--- hash2.py
+++ hash2.py
@@ -3,3 +3,10 @@
 #What was the temperature on Jan 9?
 #What was the temperature on Jan 4?
 #Figure out data structure that is best for this problem
+
+class Hashtable:
+    def __init__(self) -> None:
+        self.my_dict=None
+        
+    def dfToDict(self,DataFile):
+        pass
