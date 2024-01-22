536cd58d8eaf0f3d92cc2010ee9b32dea83a395c
franfreezy <frandelwanjawa@yahoo.com>
2024-01-22 20:24:39 +0300

    additional

--- hash2.py
+++ hash2.py
@@ -3,10 +3,21 @@
 #What was the temperature on Jan 9?
 #What was the temperature on Jan 4?
 #Figure out data structure that is best for this problem
+import pandas as pd
 
 class Hashtable:
     def __init__(self) -> None:
         self.my_dict=None
         
     def dfToDict(self,DataFile):
-        pass
+        df=pd.read_csv(datafile)
+        df=df.rename(columns={'temperature(F)':'temp'})
+        self.my_dict=dict(zip(df["date"],df["temp"]))
+        print(self.my_dict)
+
+
+if __name__=='__main__':
+    item=Hashtable()
+    datafile="nyc_weather.csv"
+    item.dfToDict(datafile)
+    
