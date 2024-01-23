0ccc0250ad11ea03b73df0c3515173fccf75c99e
franfreezy <frandelwanjawa@yahoo.com>
2024-01-23 22:17:32 +0300

    hash in built funtion falls flat(hot-air)

--- hash6.py
+++ hash6.py
@@ -5,8 +5,14 @@
 class hashtable:
     def __init__(self):
         self.max_value=10
-        self.keysArr=[[] is none for i in range(self.max_value)]
+        self.keysArr=[[] is None for i in range(self.max_value)]
     
     def getHash(self,keysArr):
         for key in keysArr:
+            #hash(key)- the notion that we have an inbuilt hash function falls flat
+            print()
 
+if __name__=='__main__':
+    item=hashtable()
+    keysArr=[12, 18, 13, 2, 3, 23, 5 , 15]
+    item.getHash(keysArr)
