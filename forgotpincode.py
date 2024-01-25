ef8637a508b3c48e3ebb4f1831564aa5d9d166a3
franfreezy <frandelwanjawa@yahoo.com>
2024-01-25 23:44:54 +0300

    better

--- forgotpincode.py
+++ forgotpincode.py
@@ -3,19 +3,27 @@ class passcode:
         self.max_value=10000
         self.possiblearr1=[None for i in range(int(self.max_value/10))]
         self.possiblearr2=[None for i in range(int((2*self.max_value)/10))]
-        self.possiblearr3=[None for i in range(int(self.max_value/10))]
-        self.possiblearr4=[None for i in range(int(self.max_value/10))]
-        self.possiblearr5=[None for i in range(int(self.max_value/10))]
-        self.possiblearr6=[None for i in range(int(self.max_value/10))]
-        self.possiblearr7=[None for i in range(int(self.max_value/10))]
-        self.possiblearr8=[None for i in range(int(self.max_value/10))]
-        self.possiblearr9=[None for i in range(int(self.max_value/10))]
-        self.possiblearr10=[None for i in range(int(self.max_value/10))]
+        self.possiblearr3=[None for i in range(int((3*self.max_value)/10))]
+        self.possiblearr4=[None for i in range(int((4*self.max_value)/10))]
+        self.possiblearr5=[None for i in range(int((5*self.max_value)/10))]
+        self.possiblearr6=[None for i in range(int((6*self.max_value)/10))]
+        self.possiblearr7=[None for i in range(int((7*self.max_value)/10))]
+        self.possiblearr8=[None for i in range(int((8*self.max_value)/10))]
+        self.possiblearr9=[None for i in range(int((9*self.max_value)/10))]
+        self.possiblearr10=[None for i in range(int((10*self.max_value)/10))]
     def possible(self):
         for i in range(int(self.max_value/10)):
             self.possiblearr1[i]=i
-        print(str(self.max_value)+ ' trials ')
-        print(self.possiblearr1)
+        for i in range(int(self.max_value/10),int((2*self.max_value)/10)):
+            self.possiblearr2[i]=i
+        for i in range(int(self.max_value/10)):
+            self.possiblearr1[i]=i
+        for i in range(int(self.max_value/10)):
+            self.possiblearr1[i]=i
+        for i in range(int(self.max_value/10)):
+            self.possiblearr1[i]=i
+        print('next')
+        print(self.possiblearr2)
 
 
 if __name__=='__main__':
