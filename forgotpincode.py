07c014f72479f03c10a8f294a7771283706c1568
franfreezy <frandelwanjawa@yahoo.com>
2024-01-26 00:06:18 +0300

    additional

--- forgotpincode.py
+++ forgotpincode.py
@@ -26,14 +26,50 @@ class passcode:
         i10=9000
         while count!=1000:
             self.possiblearr1[count]=i1
+            self.possiblearr2[count]=i2
+            self.possiblearr3[count]=i3
+            self.possiblearr4[count]=i4
+            self.possiblearr5[count]=i5
+            self.possiblearr6[count]=i6
+            self.possiblearr7[count]=i7
+            self.possiblearr8[count]=i8
+            self.possiblearr9[count]=i9
+            self.possiblearr10[count]=i10
             count+=1
             i1+=1
+            i2+=1
+            i3+=1
+            i4+=1
+            i5+=1
+            i6+=1
+            i7+=1
+            i8+=1
+            i9+=1
+            i10+=1
 
         
             
         
         print('next')
         print(self.possiblearr1)
+        print('next')
+        print(self.possiblearr2)
+        print('next')
+        print(self.possiblearr3)
+        print('next')
+        print(self.possiblearr4)
+        print('next')
+        print(self.possiblearr5)
+        print('next')
+        print(self.possiblearr6)
+        print('next')
+        print(self.possiblearr7)
+        print('next')
+        print(self.possiblearr8)
+        print('next')
+        print(self.possiblearr9)
+        print('next')
+        print(self.possiblearr10)
 
 
 if __name__=='__main__':
