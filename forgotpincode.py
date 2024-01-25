5af365c53ce553745aa679111d80ed69cec2fdf1
franfreezy <frandelwanjawa@yahoo.com>
2024-01-25 23:32:22 +0300

    change of tact

--- forgotpincode.py
+++ forgotpincode.py
@@ -1,8 +1,16 @@
 class passcode:
     def __init__(self):
         self.max_value=10000
-        self.possiblearr=[None for i in range(self.max_value)]
-    
+        self.possiblearr1=[None for i in range(self.max_value/10)]
+        self.possiblearr2=[None for i in range(self.max_value/10)]
+        self.possiblearr3=[None for i in range(self.max_value/10)]
+        self.possiblearr4=[None for i in range(self.max_value/10)]
+        self.possiblearr5=[None for i in range(self.max_value/10)]
+        self.possiblearr6=[None for i in range(self.max_value/10)]
+        self.possiblearr7=[None for i in range(self.max_value/10)]
+        self.possiblearr8=[None for i in range(self.max_value/10)]
+        self.possiblearr9=[None for i in range(self.max_value/10)]
+        self.possiblearr10=[None for i in range(self.max_value/10)]
     def possible(self):
         for i in range(self.max_value):
             self.possiblearr[i]=i
