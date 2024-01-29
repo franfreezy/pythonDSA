043b8057fb415dfe685d350067f3fde29e4f682f
franfreezy <frandelwanjawa@yahoo.com>
2024-01-29 13:17:49 +0300

    matrix rotation

--- hash12.py
+++ hash12.py
@@ -1,4 +1,4 @@
-#might not be a hash map
+
 #You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
 
 #You have to rotate the image in-place, which means you have to modify the input 2D 
@@ -6,3 +6,10 @@
 
 #Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 #Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+class matrixRotation:
+    def __init__(self, inmatrix):
+        self.inmatrix=inmatrix
+
+    def rotate(self):
+        pass
