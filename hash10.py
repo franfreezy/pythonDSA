e21ae9b1a5d175c68039b4af034242c833f30331
franfreezy <frandelwanjawa@yahoo.com>
2024-01-29 11:58:32 +0300

    ambigous if you ask me!

--- hash10.py
+++ hash10.py
@@ -9,3 +9,22 @@
 #For given hash functions, we have calculated hash values for keys 0 to 9 as: 
 #As we can see from the table, i^3 mod10 is distributing evenly from indexes 0 to 9. 
 # Other functions have not utilized all indexes. 
+
+## the implementation
+class hashtable:
+    def __init__(self,multiplier):
+        self.multiplier=multiplier
+        self.max_value=10
+        self.myArr=[[] for i in range(self.max_value)]
+    
+    def table(self):
+        for i in self.multiplier:
+            h=i%self.max_value
+            self.myArr[h].append(i)
+        print(self.myArr)
+
+if __name__== '__main__':
+    multiplier=range(2020)
+    i=multiplier
+    item=hashtable(i)
+    item.table()
