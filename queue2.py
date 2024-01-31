f9db07ddd77f6bd91641ba12e25ea2a8641fd211
franfreezy <frandelwanjawa@yahoo.com>
2024-01-31 22:26:34 +0300

    makes sense

--- queue2.py
+++ queue2.py
@@ -16,12 +16,46 @@
 #You also need to add front() function in queue class that can return the front element 
 # in the queue.
 
-class queue:
-   def __init__(self):
-      self.container=deque()
+from collections import deque
 
-   def front(self):
-      if len(self.container)==0:
-         print('empty queue')
-         return
-      return self.container.pop()
+class Queue:
+    def __init__(self):
+        self.buffer = deque()
+
+    def enqueue(self, val):
+        self.buffer.appendleft(val)
+        return self.buffer
+
+    def dequeue(self):
+        if len(self.buffer)==0:
+            print("Queue is empty")
+            return
+
+        return self.buffer.pop()
+
+    def is_empty(self):
+        return len(self.buffer) == 0
+
+    def size(self):
+        return len(self.buffer)
+
+    def front(self):
+        return self.buffer[-1]
+
+def produce_binary_numbers(n):
+    numbers_queue = Queue()
+    numbers_queue.enqueue("1")
+
+    for i in range(n):
+        front = numbers_queue.front()
+        #print("   ", front)
+        
+        numbers_queue.enqueue(front + "0")
+        
+        numbers_queue.enqueue(front + "1")
+        
+        numbers_queue.dequeue()
+
+
+if __name__ == '__main__':
+    produce_binary_numbers(10)
