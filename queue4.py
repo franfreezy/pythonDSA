b2737afb1d6458a70b2bf14b544cd49841705902
franfreezy <frandelwanjawa@yahoo.com>
2024-02-01 12:12:16 +0300

    qn4 queue

--- queue4.py
+++ queue4.py
@@ -1,3 +1,7 @@
 #1. Write a py program to implement a queue using an array with enqueue and dequeue operations. 
 # Find the top element of the stack and check if the stack is empty, full or not.
 
+
+class queue:
+    def __init__(self):
+        pass
