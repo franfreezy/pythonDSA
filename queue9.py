5a8e9a660ab4c46fba569090c69a69a2a769af03
franfreezy <frandelwanjawa@yahoo.com>
2024-02-01 22:47:29 +0300

    even sized queue

--- queue9.py
+++ queue9.py
@@ -1,9 +1,22 @@
 #6. Write a py program to find the median of all elements of a queue.
 
-from queue7 import queue
+from queue5 import queue
 
 class Queue(queue):
     def medianFind(self):
         size=self.size()
-        print(size)
+        if size%2==0:
+            middle=int(size/2)
+            print(str(self.container[middle])+ ' and '+str(self.container[middle-1]) )
 
+
+item=Queue()
+
+if __name__=='__main__':
+    item.enqueue(23)
+    item.enqueue(4)
+    item.enqueue(5)
+    item.enqueue(7)
+    item.enqueue(8)
+    print(item.enqueue(7))
+    item.medianFind()
