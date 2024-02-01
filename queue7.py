4a67705cb2876ef5ee8ce548e60dc3a0c562f3b9
franfreezy <frandelwanjawa@yahoo.com>
2024-02-01 21:46:26 +0300

    done

--- queue7.py
+++ queue7.py
@@ -1,3 +1,27 @@
 #4. Write a py program to find the sum of all elements of a queue.
 
+from collections import deque
+class queue:
+    def __init__(self):
+        self.container=deque()
 
+    def enqueue(self,data):
+        self.container.appendleft(data)
+        return list(self.container)
+    
+    def summation(self):
+        sum=0
+        for value in self.container:
+            sum+=value
+        return sum
+
+item =queue()
+
+if __name__ =='__main__':
+    item.enqueue(23)
+    item.enqueue(4)
+    item.enqueue(5)
+    item.enqueue(7)
+    item.enqueue(8)
+    print(item.enqueue(7))
+    print(item.summation())
