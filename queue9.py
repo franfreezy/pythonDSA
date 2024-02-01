2f3738170b1047920591f01adc80d279bb5a5489
franfreezy <frandelwanjawa@yahoo.com>
2024-02-01 22:39:18 +0300

    additional

--- queue9.py
+++ queue9.py
@@ -1,3 +1,9 @@
 #6. Write a py program to find the median of all elements of a queue.
 
+from queue7 import queue
+
+class Queue(queue):
+    def medianFind(self):
+        size=self.size()
+        print(size)
 
