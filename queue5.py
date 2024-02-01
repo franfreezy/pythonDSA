33fa92d7fb08d9734756dcd7311a1d77c3f10de6
franfreezy <frandelwanjawa@yahoo.com>
2024-02-01 14:57:01 +0300

    Done

--- queue5.py
+++ queue5.py
@@ -8,20 +8,41 @@ class queue:
     def enqueue(self,value):
         self.container.appendleft(value)
         return list(self.container)
-
+    
     def dequeue(self):
-        if self.size==0:
+        if self.size()==0:
             return 'queue is empty'
 
         self.value=self.container.pop()
-        return self.value
+        return list(self.container)
+
     def size(self):
         return len(self.container)
 
     def reverse(self):
-        pass
+        self.container_copy=list(self.container)
+        while self.size()!=0:
+            self.dequeue()
+            
+            
+        
+        for value in self.container_copy:
+            self.enqueue(value)
+            
+        return list(self.container)
+       
+
 item=queue()
 
 
 if __name__=='__main__':
-    pass
+    print(item.dequeue())
+    item.enqueue(23)
+    item.enqueue(5)
+    item.enqueue(2)
+   
+    item.enqueue(23)
+    print(item.enqueue(20))
+    
+    print(item.reverse())
+    
