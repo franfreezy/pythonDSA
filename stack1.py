98d9feaf9d25413671e35fcc202d0298b8c6b91f
franfreezy <frandelwanjawa@yahoo.com>
2024-02-02 10:34:18 +0300

    stack is LIFO

--- stack1.py
+++ stack1.py
@@ -3,3 +3,12 @@
 #reverse_string("We will conquere COVID-19") should return "91-DIVOC ereuqnoc lliw eW"
 
 from collectons import deque
+
+class stack:
+    def __init__(self):
+        self.container=deque()
+
+    def enqueue(self,data):
+        self.container.append(data)
+
+        return list(self.container)
