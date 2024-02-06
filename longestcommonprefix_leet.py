715dfe94d965644a2b90ec9d407b2085f32bfa44
franfreezy <frandelwanjawa@yahoo.com>
2024-02-06 19:54:10 +0300

    geting close

--- longestcommonprefix_leet.py
+++ longestcommonprefix_leet.py
@@ -29,20 +29,31 @@ class Solution:
         key=strs[0][0]
         size=len(strs)
         
-        while len(my_dict)<=1:
-
+        while len(my_dict)<=1 and i<3:
+            #print(key)
             for str in strs:
                 if str[i]==key:
                     
                     my_dict[key].append(str)
+                    
+                else:
+                    if str[i]!=key:
+                        print('" "')
+                        break
+                    if key==str[0:i]:
+                        print(key)
 
-            i+=1    
+
                 
+
+            i+=1  
+            key+=str[i]
+            #print(key)    
                 
                 
             
             print (my_dict)
-            break
+            
         
             
         
