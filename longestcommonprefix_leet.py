1fa0e3f3d0e7f82eb9589b1acedf2c06cb699156
franfreezy <frandelwanjawa@yahoo.com>
2024-02-07 13:34:31 +0300

    not efficient enough--primitive

--- longestcommonprefix_leet.py
+++ longestcommonprefix_leet.py
@@ -29,33 +29,45 @@ class Solution:
         key=''
         if len(strs)<1: #takes care of empty arrays
             return ''
+        if len(strs[0])<1: #takes care of empty strings
+            return ''
         if len(strs)==1: # takes care of one with one element
             return strs[0]
-            
+           
         
         else:
+            
             while  i<len(strs[0]):
                 key+=strs[0][i]
                 
                 
                 for str in strs:
                     string=str[0:i+1]
-                    
-                    
+                    print('here')
+                    print(str)
+                    if str=="":
+                        return ""
+
                     if key==string:
                         
                         my_dict[key].append(str)
+                        print(my_dict)
+                   
+                        
                         
                         
                 size=len(my_dict[key])
                 if size==len(strs):
                     i+=1
                 else:
+                    if i==0:
+                        return ""
+                    
                     value=list(my_dict.keys())
                     return value[-2]
                 
-            value=list(my_dict.keys())
-            return value[-1]
+            #value=list(my_dict.keys())
+            #return value[-1]
                 
                 
                 
@@ -71,10 +83,11 @@ class Solution:
         
         
 #strs = ["car","cir"]
-strs =["flower","flow","flight"]
+#strs =["flower","flow","flight"]
 #strs =["a", "a"]
 #strs =[ "a"]
-#strs = ["dog","racecar","car"]
+strs = ["dog","racecar","car"]
+strs =["",""]
 #strs =["flower","flower","flower","flower"]
 item=Solution()
 print(item.longestCommonPrefix(strs))
