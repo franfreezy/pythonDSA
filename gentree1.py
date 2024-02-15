4c6350ee2db0873334b51960d8e908aedb528038
franfreezy <frandelwanjawa@yahoo.com>
2024-02-15 22:32:52 +0300

    COETEC TREE

--- gentree1.py
+++ gentree1.py
@@ -3,3 +3,27 @@ class GeneralTree:
         self.data=data
         self.children=[]
         self.parent=None
+
+    def addChild(self,child):
+        child.parent=self
+        self.children.append(child)
+        
+
+    def Print(self):
+        print(self.data)
+        if self.children:
+            for child  in self.children:
+                child.Print()
+
+if __name__=='__main__':
+    school=GeneralTree("COETEC")
+    
+    EEE=GeneralTree("EEE")
+    EEE.addChild(GeneralTree("EEE"))
+    EEE.addChild(GeneralTree("ECE"))
+    TIE=GeneralTree("TIE")
+    TIE.addChild(GeneralTree("TIE"))
+    school.addChild(EEE)
+    school.addChild(TIE)
+    school.Print()
+
