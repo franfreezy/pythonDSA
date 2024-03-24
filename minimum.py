a6ac7f282f9090eb729b212337542ddb4a946d68
franfreezy <frandelwanjawa@yahoo.com>
2024-03-24 22:31:24 +0300

    additional

--- minimum.py
+++ minimum.py
@@ -1,4 +1,26 @@
 arr=[3,16,1,6,2]
 
+arr=[4,6,1,4,1,6]
 arr.sort()
 print(len(arr))
+
+prefix=[]
+minimumu_cost=0
+firstvalue=arr[0]
+i=0
+count=0
+while firstvalue==arr[i]:
+    
+    prefix.append(arr[i])
+    i+=1
+    count+=1
+prefix.append(arr[i])
+next_val=arr[i]
+
+
+cost=firstvalue-next_val
+
+print(prefix) 
+print(cost)
+arr=[cost+element for element in arr]
+print(arr)
--- tasks.py
+++ tasks.py
@@ -1,1 +1,2 @@
 #minimum number of machines
+
