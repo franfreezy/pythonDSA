4f7d7695e0ec3c0abc6693435f45028dd8a0512c
franfreezy <frandelwanjawa@yahoo.com>
2024-03-24 23:24:55 +0300

    additional

--- minimum.py
+++ minimum.py
@@ -2,25 +2,36 @@ arr=[3,16,1,6,2]
 
 arr=[4,6,1,4,1,6]
 arr.sort()
-print(len(arr))
 
-prefix=[]
-minimumu_cost=0
-firstvalue=arr[0]
-i=0
-count=0
-while firstvalue==arr[i]:
+equal_elements=all(element==arr[0] for element in arr )
+while equal_elements==False:
+    print(arr)
+    # get the max==last element after sort
+    max_val=max(arr)
+    min_val=min(arr)
+    ##getting the prefix
+    prefix=[]
+    i=0
+
+
+
+
+
+
+    while arr[i]!=max_val:
+        prefix.append(arr[i])
+        i+=1
     
-    prefix.append(arr[i])
-    i+=1
-    count+=1
-prefix.append(arr[i])
-next_val=arr[i]
+    cost=max_val-prefix[-1]
+    prefix=[cost+element for element in prefix ]
+
+    count=arr.count(max_val)
+    while count!=0:
+        prefix.append(max_val)
+        count-=1
+
+    arr=prefix
+    print(prefix) 
+    print(cost)
 
 
-cost=firstvalue-next_val
-
-print(prefix) 
-print(cost)
-arr=[cost+element for element in arr]
-print(arr)
